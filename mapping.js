f33e4efe189d0f5a5b0c5369c359596ff506937a
Helen <howareyouhelen@hotmail.com>
2017-06-29 22:38:58 +0000

    call map function and generate result in an array

---
+++ mapping.js
@@ -0,0 +1,18 @@
+var words = ["ground", "control", "to", "major", "tom"];
+
+var a = map(words, function(word) {
+  return word.length
+});
+
+function map(arr, callback) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    callback(arr[i]).push
+    return result;
+  }
+}
+
+console.log(a)
+
+[6, 7, 2, 5, 3]
+
